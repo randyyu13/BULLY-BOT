27945bf421b6970e2cebebdd08bb96ff7642f63a
randyyu13 <ldyu999@gmail.com>
2024-03-15 10:42:57 -0500

    remove file to free ram

--- misc_utils.py
+++ misc_utils.py
@@ -1,8 +1,10 @@
 from gcp_proxy import *
+import os
 
 def write_array_to_file(array, filename):
     with open(filename, 'w') as file:
         for item in array:
             file.write(str(item) + '\n')
     upload_to_gcs('plays-bucket', filename, filename, "google-credentials.json")
+    os.remove(filename)
 
