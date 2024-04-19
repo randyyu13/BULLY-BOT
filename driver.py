55ffe37455ad5477fba13bbe19952ea9bdad35ea
randyyu13 <ldyu999@gmail.com>
2024-04-19 16:39:39 -0500

    restarted hot fix

--- driver.py
+++ driver.py
@@ -41,26 +41,46 @@ async def look_for_tweet_update():
             await post_most_recent_tweets(tweet_blob)
 
 async def post_most_recent_lines(blob, minutes):
-    all_lines = blob.download_as_text()
-    # embed = Embed(
-    #     color = Colour.dark_purple(),
-    #     description=all_lines
-    # )
-    # embed.set_author(name="Plays of the Hour", icon_url='https://s3.us-west-1.amazonaws.com/redwood-labs/showpage/uploads/images/e9c2fa72-aee2-4782-9d90-e7113cad3424.png')
-    # embed.set_footer(text = f'Last updated {minutes} minutes ago')
+    all_lines = blob.download_as_text().split('\n')
+    half_index = len(all_lines) / 2
+    half_index = int(half_index)
+    first_half_lines = ""
+    second_half_lines = ""
+    for i in range(0, half_index):
+        first_half_lines += all_lines[i] + "\n"
+
+    for i in range(half_index, len(all_lines)):
+        second_half_lines += all_lines[i] + "\n"
+
+    embed_1 = Embed(
+        color = Colour.dark_purple(),
+        description=first_half_lines
+    )
+    embed_1.set_author(name="Plays of the Hour", icon_url='https://s3.us-west-1.amazonaws.com/redwood-labs/showpage/uploads/images/e9c2fa72-aee2-4782-9d90-e7113cad3424.png')
+    embed_1.set_footer(text = f'Last updated {minutes} minutes ago')
+
+    embed_2 = Embed(
+        color = Colour.dark_purple(),
+        description=second_half_lines
+    )
+    embed_2.set_author(name="Plays of the Hour", icon_url='https://s3.us-west-1.amazonaws.com/redwood-labs/showpage/uploads/images/e9c2fa72-aee2-4782-9d90-e7113cad3424.png')
+    embed_2.set_footer(text = f'Last updated {minutes} minutes ago')
+
     for curr_guild in bot.guilds:
         # sports betting channel
         channel = utils.get(curr_guild.channels, name="sports-betting-bot")
-        if(contains_lock(all_lines)):
-            try:
-                role = utils.get(curr_guild.roles,name="gamblers")
-                await channel.send(f'{role.mention}')
-            except:
-                print("role not found")
-        else:
-            print("does not contain lock")
+        # if(contains_lock(all_lines)):
+        #     try:
+        #         role = utils.get(curr_guild.roles,name="gamblers")
+        #         await channel.send(f'{role.mention}')
+        #     except:
+        #         print("role not found")
+        # else:
+        #     print("does not contain lock")
         # await channel.send(embed=embed)
-        await channel.send(f'```{all_lines}```')
+        
+        await channel.send(embed=embed_1)
+        await channel.send(embed=embed_2)
 
 async def post_most_recent_tweets(blob):
     tweets_json = json.loads(blob.download_as_text())
